6a89591db615bacc7302a376b9d5f57646fd4428
Jiri Popelka <jpopelka@redhat.com>
2017-05-09 12:51:12 +0200

    Fix the 'Forbidden Phrase' issue in oslc

--- get_oslc.sh
+++ get_oslc.sh
@@ -6,5 +6,5 @@ dnf -y copr enable jpopelka/license-check
 dnf -y install oslc
 
 # remove some weird license matches that produce too many false positives
-rm -f /usr/share/oslc-3.0/licenses/{crc32,diffmark}.*
+rm -f /usr/share/oslc-3.0/licenses/{crc32,diffmark,license-f,copyright-f,clearsilver}.*
 
